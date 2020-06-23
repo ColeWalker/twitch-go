dd5f60da35265ca492ee44cd0ea914d4b80ffcf5
ColeWalker <colegrwalker@gmail.com>
2020-06-23 16:22:17 -0400

    successfully able to connect and read chat

--- bot.go
+++ bot.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
+	"net/textproto"
 	"os"
+	"strings"
+	"time"
 )
 
 //AccessToken stores a single access token
@@ -15,6 +19,7 @@ type AccessToken struct {
 	Token string `json:"access_token"`
 }
 
+//The Bot object which stores websocket info
 type Bot struct {
 	server  string
 	port    string
@@ -24,9 +29,40 @@ type Bot struct {
 	conn    net.Conn
 }
 
-func main() {
-	token := refreshAuth(os.Getenv("twitch-bot-refresh-token"), os.Getenv("twitch-bot-client-id"), os.Getenv("twitch-bot-client-secret"))
-	fmt.Println(token)
+func newBot() *Bot {
+	return &Bot{
+		server:  "irc.twitch.tv",
+		port:    "6667",
+		nick:    "supcole",
+		channel: "#supcole",
+		mods:    make(map[string]bool),
+		conn:    nil}
+}
+
+//Connect bot to IRC server
+func (bot *Bot) Connect(token string) {
+	var err error
+	fmt.Println("Attempting to connect to Twitch IRC server!")
+	bot.conn, err = net.Dial("tcp", bot.server+":"+bot.port)
+
+	if err != nil {
+		fmt.Printf("Unable to connect to Twitch IRC server! Reconnecting in 10 seconds...\n")
+		time.Sleep(10 * time.Second)
+		bot.Connect(token)
+	}
+	fmt.Printf("Connected to IRC server %s\n", bot.server)
+
+	fmt.Fprintf(bot.conn, "USER %s 8 * :%s\r\n", bot.nick, bot.nick)
+	fmt.Fprintf(bot.conn, "PASS oauth:%s\r\n", token)
+	fmt.Fprintf(bot.conn, "NICK %s\r\n", bot.nick)
+	fmt.Fprintf(bot.conn, "JOIN %s\r\n", bot.channel)
+}
+
+//Message sent to server
+func (bot *Bot) Message(message string) {
+	if message != "" {
+		fmt.Fprintf(bot.conn, "PRIVMSG "+bot.channel+" :"+message+"\r\n")
+	}
 }
 
 func refreshAuth(refreshToken string, clientID string, secret string) string {
@@ -51,7 +87,7 @@ func refreshAuth(refreshToken string, clientID string, secret string) string {
 	err = json.Unmarshal(body, &NewToken)
 
 	if err != nil {
-		log.Println("Error in Unmarshall")
+		log.Println("Error in Unmarshal")
 		log.Fatalln(err)
 	}
 
@@ -59,4 +95,35 @@ func refreshAuth(refreshToken string, clientID string, secret string) string {
 		log.Fatalln(string(body))
 	}
 	return NewToken.Token
+}
+
+func main() {
+	token := refreshAuth(os.Getenv("twitch-bot-refresh-token"), os.Getenv("twitch-bot-client-id"), os.Getenv("twitch-bot-client-secret"))
+	fmt.Println(token)
+
+	bot := newBot()
+	bot.Connect(token)
+
+	defer bot.conn.Close()
+
+	reader := bufio.NewReader(bot.conn)
+	tp := textproto.NewReader(reader)
+
+	for {
+		line, err := tp.ReadLine()
+		if err != nil {
+			break
+		} else if strings.Contains(line, "PING") {
+			fmt.Fprintf(bot.conn, "PONG :tmi.twitch.tv")
+		} else if strings.Contains(line, ".tmi.twitch.tv PRIVMSG "+bot.channel) {
+			messageContents := strings.Split(line, ".tmi.twitch.tv PRIVMSG "+bot.channel)
+			// username := strings.Split(messageContents[0], "@")[1]
+			message := messageContents[1][2:len(messageContents[1])]
+			// fmt.Println(username + ": " + message)
+			if strings.Contains(message, "!oops") {
+
+			}
+			fmt.Println(line)
+		}
+	}
 }
