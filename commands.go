09c72d260742969629feecc41e3bbab5f4ac99df
ColeWalker <colegrwalker@gmail.com>
2020-07-02 18:37:02 -0400

    fixed bug where space was added at start of add1

--- commands.go
+++ commands.go
@@ -8,10 +8,10 @@ import (
 //CommandInterpreter receives username and message and interprets commands
 func (bot *Bot) CommandInterpreter(wheel *Wheel, username string, message string, moderator bool) {
 	username = strings.ToLower(username)
-	message = strings.ToLower(message)
+	lowerMessage := strings.ToLower(message)
 
-	if strings.HasPrefix(message, "!add1 ") {
-		game := strings.TrimPrefix(message, "!add1")
+	if strings.HasPrefix(lowerMessage, "!add1 ") {
+		game := strings.TrimPrefix(message, "!add1 ")
 
 		success := wheel.ChatAdd(username, game, 1)
 		if success {
@@ -21,7 +21,7 @@ func (bot *Bot) CommandInterpreter(wheel *Wheel, username string, message string
 		}
 
 		fmt.Printf("%+v\n", wheel)
-	} else if strings.HasPrefix(message, "!add2 ") {
+	} else if strings.HasPrefix(lowerMessage, "!add2 ") {
 		game := strings.TrimPrefix(message, "!add2 ")
 
 		success := wheel.ChatAdd(username, game, 2)
@@ -31,7 +31,7 @@ func (bot *Bot) CommandInterpreter(wheel *Wheel, username string, message string
 			bot.Message("Add unsuccessful")
 		}
 		fmt.Printf("%+v\n", wheel)
-	} else if strings.HasPrefix(message, "!add3 ") {
+	} else if strings.HasPrefix(lowerMessage, "!add3 ") {
 		game := strings.TrimPrefix(message, "!add3 ")
 
 		success := wheel.ChatAdd(username, game, 3)
@@ -43,9 +43,10 @@ func (bot *Bot) CommandInterpreter(wheel *Wheel, username string, message string
 		}
 
 		fmt.Printf("%+v\n", wheel)
-	} else if strings.HasPrefix(message, "!geturl") && moderator {
+	} else if strings.HasPrefix(lowerMessage, "!geturl") && moderator {
 		reply := wheel.GetURL()
-		bot.Message(reply)
+		fmt.Println(reply)
+		go bot.Message(reply)
 		fmt.Println("get url called")
 	}
 	//TODO -> !geturl command for mods only
